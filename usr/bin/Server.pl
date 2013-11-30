5aa6df2db1c15dc322a9036ebe74bff9ef775df1
Anmol Porwal <anmolporwal@ymail.com>
2013-11-30 23:20:38 +0530

    Create Server.pl

---
+++ usr/bin/Server.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use IO::Socket::INET;
+
+my $socket;
+my  $clientsocket;
+my $serverdata;
+my $clientdata;
+
+$socket = new IO::Socket::INET (
+    LocalHost => '127.0.0.1',
+    LocalPort => '0155',
+    Proto => 'tcp',
+    Listen => 1,
+    Reuse => 1
+) or die "Oops: $! \n";
+print "Waiting for the Client.\n";
+
+
+$clientsocket = $socket->accept();
+
+print   "Connected from : ", $clientsocket->peerhost();     # Display messages
+print   ", Port : ", $clientsocket->peerport(), "\n";
+
+# Write some data to the client  
+$serverdata = "This is the Server speaking :)\n";
+print $clientsocket "$serverdata \n";
+
+# read the data from the client
+while($clientdata = <$clientsocket>)
+{
+if(($clientdata eq "quit") or ($clientdata eq ""))
+{
+$socket->close();
+}
+else
+{
+print "Message received from Client : $clientdata\n";
+}
+}
+#$socket->close();
